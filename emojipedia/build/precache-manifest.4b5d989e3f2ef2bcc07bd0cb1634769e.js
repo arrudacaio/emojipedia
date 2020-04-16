4e3e32792a080643e4f79ce971c977b327c47974
arrudacaio <caio.arruda@ccc.ufcg.edu.br>
2020-04-16 20:30:39 -0300

    finished

---
+++ emojipedia/build/precache-manifest.4b5d989e3f2ef2bcc07bd0cb1634769e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36f73495de7cee67dbf83dc2de493d63",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5669a793b8dd590779d1",
+    "url": "/static/js/2.a68e60e1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a68e60e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db9d1495b0827f0dafd8",
+    "url": "/static/js/main.5ef22172.chunk.js"
+  },
+  {
+    "revision": "60a675185329cc6bac66",
+    "url": "/static/js/runtime-main.5fa90bce.js"
+  }
+]);
